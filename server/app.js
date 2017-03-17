b24a94b42e80f26c8082dc84971509976c8245ad
Wolfgang Beyer <wolfi.b@gmx.at>
2017-03-17 15:00:14 +0100

    add unique filenames for temporary files on vg API server

--- server/app.js
+++ server/app.js
@@ -1,68 +1,66 @@
-/*jshint esversion:6 */
+/* eslint no-console: "off" */
+/* eslint strict:0 */
 
-var spawn = require('child_process').spawn;
-var express = require('express');
-var bodyParser = require('body-parser');
+'use strict';
 
-var app = express();
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+const spawn = require('child_process').spawn;
+const express = require('express');
+const bodyParser = require('body-parser');
+const uuid = require('uuid/v1');
+const fs = require('fs');
+const rl = require('readline');
+
+const app = express();
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true,
 }));
 
-//app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.post('/vg_trace', function(req, res) {
-    var nodeID = req.body.nodeID;
-    var distance = req.body.distance;
-    console.log('http POST received');
-    console.log('nodeID = ' + nodeID);
-    console.log('distance = ' + distance);
+app.post('/vg_trace', (req, res) => {
+  const nodeID = req.body.nodeID;
+  const distance = req.body.distance;
+  console.log('http POST received');
+  console.log(`nodeID = ${nodeID}`);
+  console.log(`distance = ${distance}`);
 
-    var child = spawn('vg', ['trace', '-x', './vg_data/chr22_v3.vg.xg', '-n', nodeID, '-d', distance, '-j', './vg_data/out3.json']);
+  const fileName = uuid();
+  const child = spawn('vg', ['trace', '-x', './vg_data/chr22_v3.vg.xg', '-n', nodeID, '-d', distance, '-j', `./vg_data/${fileName}.json`]);
 
-    child.stderr.on('data', (data) => {
-        console.log('err data: ' + data);
+  child.stderr.on('data', (data) => {
+    console.log(`err data: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+
+    // Read File Synchronously
+    const jsonfile = fs.readFileSync(`./vg_data/${fileName}.json`);
+    const obj = JSON.parse(jsonfile);
+
+    const lineReader = rl.createInterface({
+      input: fs.createReadStream(`./vg_data/${fileName}.json.annotation`),
     });
 
-    child.stdout.on('data', (data) => {
-        //result += data.toString();
-        //data.pipe(res);
-        console.log(data.toString());
+    let i = 0;
+    lineReader.on('line', (line) => {
+      // line = line.replace(/\s+/g, ' ');
+      const arr = line.replace(/\s+/g, ' ').split(' ');
+      if (obj.path[i].name === arr[0]) {
+        obj.path[i].freq = arr[1];
+      } else {
+        console.log('Mismatch');
+      }
+      i += 1;
     });
 
-    child.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-        //var jsonfile = require("./vg_data/out3.json");
-
-        // Read Synchronously
-        var fs = require("fs");
-        var jsonfile = fs.readFileSync("./vg_data/out3.json");
-        var obj = JSON.parse(jsonfile);
-
-        var lineReader = require('readline').createInterface({
-          input: fs.createReadStream('./vg_data/out3.json.annotation')
-        });
-
-        var i = 0;
-        lineReader.on('line', function (line) {
-          line = line.replace(/\s+/g, ' ');
-          var arr = line.split(' ');
-          if (obj.path[i].name == arr[0]) {
-            obj.path[i].freq = arr[1];
-          } else {
-            console.log('Mismatch');
-          }
-          i++;
-        });
-
-        lineReader.on('close', function () {
-          return res.json(obj);
-        });
-
+    lineReader.on('close', () => {
+      fs.unlink(`./vg_data/${fileName}.json`);
+      fs.unlink(`./vg_data/${fileName}.json.annotation`);
+      res.json(obj);
     });
+  });
 });
 
-app.listen(3000, function () {
-    console.log('TubeMapServer listening on port 3000!');
-});
+app.listen(3000, () => console.log('TubeMapServer listening on port 3000!'));
