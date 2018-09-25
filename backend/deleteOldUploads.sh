df384634e67c12e125227a4545b051cf9194df60
Wolfgang Beyer <woolfi.b@gmail.com>
2018-09-25 16:51:06 +0200

    add script for deleting old uploaded files

---
+++ backend/deleteOldUploads.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This script deletes files which have been uploaded via the web app to the
+#  ./uploads folder and which are older than 1 hour. This prevents the
+# folder from accumulating unnecessary files.
+# Automatically run this script periodically by adding a line to your
+# crontab. Run 'crontab -e' and add the following line to run the script
+# hourly (replace correct path and remove "):
+# "0 * * * * bash [substitute your path here]/sequenceTubeMap/backend/deleteOldUploads.sh"
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "${DIR}/uploads"
+touch -d '-1 hour' limit
+for f in ./*;
+do
+  if [ limit -nt "${f}" ]; then
+      rm "${f}"
+  fi
+done
