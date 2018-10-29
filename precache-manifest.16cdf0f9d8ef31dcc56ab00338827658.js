9cd806c8d053046a6fb95ad34254b9ea811b4a33
Wolfgang Beyer <woolfi.b@gmail.com>
2018-10-29 17:33:04 +0100

    Updates

---
+++ precache-manifest.16cdf0f9d8ef31dcc56ab00338827658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c36ae8ac02a8af0e0ddd",
+    "url": "/sequenceTubeMap/static/js/runtime~main.c36ae8ac.js"
+  },
+  {
+    "revision": "56f1da2f87d2ea657509",
+    "url": "/sequenceTubeMap/static/js/main.56f1da2f.chunk.js"
+  },
+  {
+    "revision": "6e8034ee0abf1f48606e",
+    "url": "/sequenceTubeMap/static/js/2.6e8034ee.chunk.js"
+  },
+  {
+    "revision": "12ef5dc9a11ff8c68f23",
+    "url": "/sequenceTubeMap/static/js/1.12ef5dc9.chunk.js"
+  },
+  {
+    "revision": "56f1da2f87d2ea657509",
+    "url": "/sequenceTubeMap/static/css/main.6bd7cea2.chunk.css"
+  },
+  {
+    "revision": "6e8034ee0abf1f48606e",
+    "url": "/sequenceTubeMap/static/css/2.d7a52674.chunk.css"
+  },
+  {
+    "revision": "78b43af8b3d745efd350520c2cd0fd8e",
+    "url": "/sequenceTubeMap/index.html"
+  }
+];
