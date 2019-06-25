d518440291398219b849164d44e3ecda9d393431
Wolfgang Beyer <woolfi.b@gmail.com>
2019-06-25 17:29:04 +0200

    Updates

---
+++ precache-manifest.28e55cddf905b529de4646245973d5a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fee766ea92c5fd16c45a9aaa0ca9cd",
+    "url": "/sequenceTubeMap/index.html"
+  },
+  {
+    "revision": "9e8baf979914df98365e",
+    "url": "/sequenceTubeMap/static/css/2.b837a2f7.chunk.css"
+  },
+  {
+    "revision": "37495a18114a8aa907c4",
+    "url": "/sequenceTubeMap/static/css/main.fdb4a4c7.chunk.css"
+  },
+  {
+    "revision": "9e8baf979914df98365e",
+    "url": "/sequenceTubeMap/static/js/2.73715943.chunk.js"
+  },
+  {
+    "revision": "5a66d5d68b780e315a4f",
+    "url": "/sequenceTubeMap/static/js/3.275d7138.chunk.js"
+  },
+  {
+    "revision": "37495a18114a8aa907c4",
+    "url": "/sequenceTubeMap/static/js/main.dce84d13.chunk.js"
+  },
+  {
+    "revision": "1527413d0dca8d097dc6",
+    "url": "/sequenceTubeMap/static/js/runtime~main.6490cc05.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sequenceTubeMap/precache-manifest.9e37fc7a8b7c021794641ecdb80382d1.js"
+  "/sequenceTubeMap/precache-manifest.28e55cddf905b529de4646245973d5a9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sequenceTubeMap/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sequenceTubeMap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
