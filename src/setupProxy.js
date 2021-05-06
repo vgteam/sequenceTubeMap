902db8bfad2d07097e2a816618d81e86511ae5f4
Adam Novak <anovak@soe.ucsc.edu>
2021-05-06 14:15:00 -0700

    Secure dev setup (#128)
    * Add a way for the server to bind to a particular address
    
    * Update packages and allow restricting dev setup to local address
    
    * Allow environment to set dev server port and host
    
    * Sort path names
    
    * Fix web socket proxying with new middleware version and ports
    
    * Adjust fonts for better Inkscape compatibility
    
    * Update yarn.lock by installing with a new enough npm to respect it
    
    * Let npm audit adjust my personal modules and yarn.lock
    
    * Upgrade concurrently even though that won't make NPM shut up about the dangers of polynomial time regex implementations
    
    * Take only the webpack dev server that react scripts wants
    
    * Stop using done with async setup and teardown, which is now prohibited

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,25 @@
 // Configure the React development server to proxy both normal API calls and web socket requests.
 const proxy = require('http-proxy-middleware');
 
+const config = require('./config.json');
+
+// Determine the host part of the URL to use for a given address
+function addressToHost(address) {
+  if (address === undefined) {
+    return 'localhost';
+  }
+  if (address.includes(':')) {
+    // It's IPv6
+    return '[' + address + ']';
+  }
+  return address;
+}
+
 module.exports = function(app) {
-  // When we set up the proxy this way we don't have to do anything special for the websockets.
-  app.use(proxy('http://localhost:3000/api'));
+  let serverBase = 'http://' + addressToHost(config.serverBindAddress) + ':' + (config.serverPort || '3000');
+  app.use(proxy(serverBase + '/api'));
+  // Websockets don't seem to get through unless we ask for them explicitly.
+  // They can just go to the root because the server ignores websocket paths.
+  app.use(proxy('/api/v0', {ws: true, target: serverBase}));
 };
 
