d2bddb8a466a2a90672e919883c73ebb6fa6afdd
Adam Novak <anovak@soe.ucsc.edu>
2021-05-06 11:54:23 -0700

    Update packages and allow restricting dev setup to local address

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,22 @@
 // Configure the React development server to proxy both normal API calls and web socket requests.
 const proxy = require('http-proxy-middleware');
 
+const config = require('./config.json');
+
+// Determine the host part of the URL to use for a given address
+function addressToHost(address) {
+  if (address === undefined) {
+    return 'localhost';
+  }
+  if (address.includes(':')) {
+    // It's IPv6
+    return '[' + address + ']';
+  }
+  return address;
+}
+
 module.exports = function(app) {
   // When we set up the proxy this way we don't have to do anything special for the websockets.
-  app.use(proxy('http://localhost:3000/api'));
+  app.use(proxy('http://' + addressToHost(config.serverBindAddress) + ':' + (config.serverPort || '3000') + '/api'));
 };
 
