19f2f454e3370336bee09474289c92354852aa7d
Jean Monlong <jean.monlong@gmail.com>
2021-09-27 08:33:01 -0700

    add interface module to select the regions

---
+++ src/components/BedRegionsFormRow.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Label } from 'reactstrap';
+import SelectionDropdown from "./SelectionDropdown";
+
+class BedRegionsFormRow extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Label
+          for="regionSelect"
+          className="customData tight-label mb-2 mr-sm-2 mb-sm-0 ml-2"
+        >
+          Region:
+        </Label>
+        <SelectionDropdown
+          className="customDataMounted dropdown mb-2 mr-sm-4 mb-sm-0"
+          id="regionSelect"
+          value={this.props.regionSelect}
+          onChange={this.props.handleInputChange}
+          options={this.props.regionSelectOptions}
+        />
+
+      </React.Fragment>
+    );
+  }
+}
+
+BedRegionsFormRow.propTypes = {
+  regionSelect: PropTypes.string.isRequired,
+  regionSelectOptions: PropTypes.array.isRequired,
+  handleInputChange: PropTypes.func.isRequired
+};
+
+export default BedRegionsFormRow;
