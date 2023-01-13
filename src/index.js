aed0ba052fd0906eb09651b86004fb3f3dbd59c0
Adam Novak <anovak@soe.ucsc.edu>
2023-01-13 16:17:30 -0500

    Add a demo page at /demo where you can fiddle with components in isolation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
+import demo from "./components/RegionInput.demo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render((
+
+<BrowserRouter>
+  <Routes>
+    <Route path="/">
+      <Route index element={<App />} />
+      <Route path="demo" element={demo} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+
+), document.getElementById("root"));
