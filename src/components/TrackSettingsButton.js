56a590d5adcf943fa7b100151eda2b00e1196006
shreyasun <shreyasundar27@gmail.com>
2023-04-04 13:31:40 -0700

    Change appearance of track settings button and make popup controlled

--- src/components/TrackSettingsButton.js
+++ src/components/TrackSettingsButton.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import PropTypes from "prop-types";
 import TrackSettings from "./TrackSettings.js";
+import { Button } from 'reactstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from '@fortawesome/free-solid-svg-icons';
+
 import {
   Container,
   CardBody,
@@ -15,8 +19,17 @@ export const TrackSettingsButton = ({
     label,
     availableColors
 }) => {
+    // based off of https://react-popup.elazizi.com/controlled-popup/#using-open-prop
+    const [open, setOpen] = useState(false);
+    const close = () => setOpen(false);
     return(
-        <Popup trigger={<button type="button"> Settings </button>} position="right center" contentStyle={{width: "760px"}} modal>
+      <div>
+        
+        <Button aria-label="Settings" onClick={() => setOpen(!open)}><FontAwesomeIcon icon={faGear} /></Button>
+      
+        {/* Popup has a trigger option, but passing in a button to trigger did not allow for the popup to be opened and closed
+         with a Reactstrap button. We had to use onClick and onClose instead to open and close depending on the state of the popup */}
+        <Popup open={open} closeOnDocumentClick onClose={close} contentStyle={{width: "760px"}} modal>
           <Container>
             <Card>
               <CardBody>
@@ -30,6 +43,7 @@ export const TrackSettingsButton = ({
             </Card>
           </Container>
         </Popup>
+      </div>
     )
 }
 
---
+++ src/components/TrackSettingsButton.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import {TrackSettingsButton} from './TrackSettingsButton';
+import {screen} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+
+describe('TrackSettingsButton', () => {
+    it('opens popup', async () => {
+        const result = render(
+            <TrackSettingsButton fileType="graph" trackColorSettings={{mainPallete: "blues",
+                auxPallete: "reds",
+                colorReadsByMappingQuality: false}} setTrackColorSetting={function(a, b){}}></TrackSettingsButton>
+        );
+
+        await userEvent.click(screen.getByRole('button'));
+
+        expect(screen.getByRole('heading')).toBeTruthy();
+
+        await userEvent.click(screen.getByRole('button'));
+
+        expect(screen.queryByRole('heading')).toBeFalsy();
+
+
+    });
+});
