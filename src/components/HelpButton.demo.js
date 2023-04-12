444fdd5dd42f97a767b4851798dbc33cbc27e407
shreyasun <shreyasundar27@gmail.com>
2023-04-12 12:45:06 -0700

    Add Help Button, Help Button Demo, and sample file

---
+++ src/components/HelpButton.demo.js
@@ -0,0 +1,17 @@
+import Demo, {props as P} from 'react-demo'
+// See https://github.com/rpominov/react-demo for how to make a demo
+
+import HelpButton from "./HelpButton.js";
+
+export default (<Demo
+    props={{
+      file: P.string('./help/help.md'),
+    }}
+  >
+    {
+      (props) => {
+        return <HelpButton {...props}/>
+      }
+    }
+  </Demo>)
+  
---
+++ src/components/HelpButton.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import Popup from 'reactjs-popup';
+import PropTypes from "prop-types";
+import { Button } from 'reactstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+
+import {
+  Container,
+  CardBody,
+  Card,
+} from 'reactstrap';
+
+export const HelpButton = ({
+  file
+}) => {
+    // based off of https://react-popup.elazizi.com/controlled-popup/#using-open-prop
+    const [open, setOpen] = useState(false);
+    const close = () => setOpen(false);
+
+    // based off of https://stackoverflow.com/questions/42928530/how-do-i-load-a-markdown-file-into-a-react-component/65584658#65584658
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+      fetch(file)
+        .then(res => res.text())
+        .then(md => { setContent(md) })
+    }, [file])
+
+    return(
+      <div>
+        
+        <Button aria-label="Help" onClick={() => setOpen(!open)}><FontAwesomeIcon icon={faQuestion} /></Button>
+      
+        {/* Popup has a trigger option, but passing in a button to trigger did not allow for the popup to be opened and closed
+         with a Reactstrap button. We had to use onClick and onClose instead to open and close depending on the state of the popup */}
+        <Popup open={open} closeOnDocumentClick onClose={close} contentStyle={{width: "760px"}} modal>
+          <Container>
+            <Card>
+              <CardBody>
+                  <h2>Help</h2>
+                  {content}
+              </CardBody>
+            </Card>
+          </Container>
+        </Popup>
+      </div>
+    )
+}
+
+
+HelpButton.propTypes = {
+  file: PropTypes.string.isRequired,
+}
+
+HelpButton.defaultProps = {
+
+}
+
+export default HelpButton;
