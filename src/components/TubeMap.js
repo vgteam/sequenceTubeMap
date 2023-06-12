3332f1ae7485e75182ee952a8ee1db7be5604c4a
Adam Novak <anovak@soe.ucsc.edu>
2023-06-12 12:18:24 -0400

    Fix tube map rerendering when item info dialog opens and closes
    We now check to make sure that the tube map's determining props actually changed before going to re-render it.
    
    Also, we now no longer rewrite the color schemes in place and instead properly return a new object with the new stuff under the right key.
    
    This moves visOptions into the props of the tube map itself, because when they change the tube map needs to redraw.
    
    We might now be able to cut some redraw calls from the setters for them in tubemap.js.

--- src/components/TubeMap.js
+++ src/components/TubeMap.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import * as tubeMap from "../util/tubemap";
+import isEqual from "react-fast-compare";
 
 class TubeMap extends Component {
   componentDidMount() {
+    this.updateVisOptions();
     this.createTubeMap();
   }
 
-  componentDidUpdate() {
-    this.createTubeMap();
+  componentDidUpdate(prevProps) {
+    console.log('Props:', this.props);
+    if (!isEqual(this.props, prevProps)) {
+      console.log('Props have changed so re-creating tube map');
+      this.updateVisOptions();
+      this.createTubeMap();
+    } else {
+      console.log('Props have not changed so leaving existing tube map');
+    }
   }
 
   createTubeMap = () => {
@@ -18,8 +27,30 @@ class TubeMap extends Component {
       tracks: this.props.tracks,
       reads: this.props.reads,
       region: this.props.region,
+      visOptions: this.props.visOptions,
     });
   };
+
+  updateVisOptions() {
+    const visOptions = this.props.visOptions;
+    visOptions.compressedView
+      ? tubeMap.setNodeWidthOption(1)
+      : tubeMap.setNodeWidthOption(0);
+    tubeMap.setMergeNodesFlag(visOptions.removeRedundantNodes);
+    tubeMap.setTransparentNodesFlag(visOptions.transparentNodes);
+    tubeMap.setShowReadsFlag(visOptions.showReads);
+    tubeMap.setSoftClipsFlag(visOptions.showSoftClips);
+
+    // apply new colorReadsByMappingQuality value to all tracks
+    // to be changed, add options to change colorReadsByMappingQuality individually
+    tubeMap.setColorReadsByMappingQualityFlag(visOptions.colorReadsByMappingQuality);
+
+    for (let i = 0; i < visOptions.colorSchemes.length; i++) {
+      // update tubemap colors
+      tubeMap.setColorSet(i, visOptions.colorSchemes[i]);
+    }
+    tubeMap.setMappingQualityCutoff(visOptions.mappingQualityCutoff);
+  }  
 
   render() {
     return <svg id="svg" alt="Rendered sequence tube map visualization" />;
@@ -31,6 +62,7 @@ TubeMap.propTypes = {
   tracks: PropTypes.array.isRequired,
   reads: PropTypes.array.isRequired,
   region: PropTypes.array.isRequired,
+  visOptions: PropTypes.object.isRequired
 };
 
 export default TubeMap;
