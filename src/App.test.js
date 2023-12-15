55103c7d88bffb23256064e655da1da69029acb3
Adam Novak <anovak@soe.ucsc.edu>
2023-12-15 18:14:40 -0500

    Allow the error render not to finish on the immediate first tick

--- src/App.test.js
+++ src/App.test.js
@@ -63,7 +63,9 @@ it("renders with error when api call to server throws", async () => {
     throw new Error("Mock Server Error");
   });
   render(<App />);
-  expect(screen.getAllByText(/Mock Server Error/i)[0]).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getAllByText(/Mock Server Error/i)[0]).toBeInTheDocument();
+  });
 });
 
 it("renders without crashing when sent bad fetch data from server", async () => {
