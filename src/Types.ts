1cb524ae92f24fd518f5533cb8a1ca1b95ea94f5
Shreya Sundar <shreyasundar27@gmail.com>
2024-02-21 09:02:55 -0800

    removed references to datapath

--- src/Types.ts
+++ src/Types.ts
@@ -1,10 +1,6 @@
 // Type file for documentation
 // (not actually enforced by Typescript)
 
-// All the paths are scoped to a type on the server side.
-// This is the type of data paths we are working with, such as "mounted", "upload", or "default" - see server.js.
-type DataPath = "mounted" | "default" | "upload";
-
 // Describes whether a built-in example, user uploaded file, mounted, or synthetic example
 // Fills input for the "Data:" dropdown in the HeaderForm
 type DataType = "built-in" | "file-upload" | "mounted files" | "examples";
@@ -34,7 +30,6 @@ type ViewTarget = {
 
   // Non-essential to server, used for examples
   name?: string;
-  dataPath: DataPath;
   dataType: DataType;
 };
 
